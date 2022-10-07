928048a235d9c3bcf0c4547f88bb9c483483efe3
yueyi <1481796592@qq.com>
2022-10-07 21:55:01 +0800

    compile il code and check

---
+++ chapter1/1.8/Test.cs
@@ -0,0 +1,34 @@
+﻿using System;
+    
+
+namespace _1._8
+{
+    internal sealed class Test
+    {
+        public Test() { }
+        ~Test() { }
+        public static Boolean operator == (Test t1,Test t2)
+        {
+            return true;
+        }
+        public static Boolean operator !=(Test t1, Test t2)
+        {
+            return false;
+        }
+        public static Test operator +(Test t1, Test t2)
+        {
+            return null;
+        }
+        public String Aproperty
+        {
+            get { return null; }
+            set { }
+        }
+        public String this[Int32 x]
+        {
+            get { return null; }
+            set { }
+        }
+        event EventHandler AnEvent;
+    }
+}
