c5acf978e84811093c41870f68425e663076b70e
yueyi <1481796592@qq.com>
2022-11-01 01:11:18 +0800

    5.5

---
+++ chapter5/5.5/Dynamic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5
+{
+    internal class Dynamic
+    {
+        static void Main()
+        {
+            dynamic target = "yueyi";
+            dynamic arg = "yue";
+            Boolean result = target.Contains(arg);
+            Console.WriteLine(result);
+        }
+    }
+}
