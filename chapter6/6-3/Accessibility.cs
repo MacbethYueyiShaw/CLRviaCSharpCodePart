c15a365bdd71effbe0e46ee89ab1f53d6ff99fa3
yueyi <1481796592@qq.com>
2022-11-02 15:12:52 +0800

    6.3

---
+++ chapter6/6-3/Accessibility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3
+{
+    internal class Accessibility
+    {
+        public void public_method()
+        {
+
+        }
+        private void private_method()
+        {
+
+        }
+        void default_method()
+        {
+
+        }
+        internal void internal_method()
+        {
+
+        }
+        protected void protected_method()
+        {
+
+        }
+        protected internal void protected_internal_method()
+        {
+
+        }
+        void test_all_method()
+        {
+            public_method();
+            private_method();
+            default_method();
+            internal_method();
+            protected_method();
+            protected_internal_method();
+        }
+    }
+}
---
+++ chapter6/6-3/entry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3
+{
+    internal class entry
+    {
+        static void Main()
+        {
+            Accessibility a = new Accessibility();
+            //a cannot access methods whose level higher than internal
+        }
+    }
+}
