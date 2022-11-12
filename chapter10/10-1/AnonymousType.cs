1cc0537fdb27a1fcaecebb06b001b1da1e0756bd
yueyi <1481796592@qq.com>
2022-11-12 16:14:47 +0800

    parse AnonymousObject

--- chapter10/10-1/AnonymousType.cs
+++ chapter10/10-1/AnonymousType.cs
@@ -11,11 +11,40 @@ namespace Project1
     {
         static void Main()
         {
+            var type = TryToParseAnonymousType();
+            Console.WriteLine(type.ToString());
+            /*var AnonymousObj = System.Activator.CreateInstance(type);
+            Console.WriteLine(AnonymousObj.GetType());
+            Console.WriteLine(AnonymousObj.ToString());*/
+            var o = new object();
+            TryToParseAnonymousObject(out o);
+            Console.WriteLine(o.GetType());
+            Console.WriteLine(o.ToString());
+            //cannot cast o to AnonymousType because we cannot access AnonymousType type name in metadata
+        }
+        static Type TryToParseAnonymousType()
+        {
             var AnonymousObject = new { Name = "name", age = 123 };
             Console.WriteLine(AnonymousObject.GetType());
             Console.WriteLine(AnonymousObject.ToString());
             Type type = AnonymousObject.GetType();
-            var AnonymousObject2 = new { Name = "name", age = 123 , Value = AnonymousObject };
+            return type;
+        }
+        static void TryToParseAnonymousObject(out object handler)
+        {
+            var AnonymousObject = new { Name = "name", age = 123 };
+            Console.WriteLine(AnonymousObject.GetType());
+            Console.WriteLine(AnonymousObject.ToString());
+            Type type = AnonymousObject.GetType();
+            handler = AnonymousObject;//implicit cast
+        }
+        void codeExample()
+        {
+            var AnonymousObject = new { Name = "name", age = 123 };
+            Console.WriteLine(AnonymousObject.GetType());
+            Console.WriteLine(AnonymousObject.ToString());
+            Type type = AnonymousObject.GetType();
+            var AnonymousObject2 = new { Name = "name", age = 123, Value = AnonymousObject };
             Console.WriteLine(AnonymousObject2.GetType());
             Console.WriteLine(AnonymousObject2.ToString());
             var AnonymousObject3 = new { Name = "name", age = 123 };
@@ -29,7 +58,7 @@ namespace Project1
                 where LastWrite > (DateTime.Now - TimeSpan.FromDays(365))
                 orderby LastWrite
                 select new { Path = pathname, LastWrite };
-            foreach(var item in query)
+            foreach (var item in query)
             {
                 Console.WriteLine(item.ToString());
             }
