ebcb59cb0324c70bc81ad99d4a4ee3974a25914b
yueyi <1481796592@qq.com>
2022-11-12 21:26:19 +0800

    10-2

---
+++ chapter10/10-2/MyDictionary.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_2
+{
+    internal class MyDictionary : IEnumerable
+    {
+        private Int64[] mNums;
+        private String[] mStrings;
+        public dynamic this[Int32 index]
+        {
+            get
+            {
+                if (index > mNums.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return new { num = mNums[index], str = mStrings[index] };
+            }
+        }
+        public Int64[] Get_mNums()
+        {
+            return mNums;
+        }
+        public String[] Get_mStrings()
+        {
+            return mStrings;
+        }
+        void Add(String str, Int64 num)
+        {
+            mNums.Append(num);
+            mStrings.Append(str);
+        }
+        public override string ToString()
+        {
+            String res = "";
+            for(Int32 i = 0; i < mNums.Length; i++)
+            {
+                res += "num:" + mNums[i].ToString() + " str:" + mStrings[i] + "\n";
+            }
+            return res;
+        }
+        public MyDictionary(Int64[] intArray, String[] StrArray)
+        {
+            mNums = new Int64[intArray.Length];
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                mNums[i] = intArray[i];
+            }
+
+            mStrings = new String[StrArray.Length];
+            for (int i = 0; i < StrArray.Length; i++)
+            {
+                mStrings[i] = StrArray[i];
+            }
+        }
+
+        // Implementation for the GetEnumerator method.
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)GetEnumerator();
+        }
+
+        public MyDictionaryEnum GetEnumerator()
+        {
+            return new MyDictionaryEnum(mNums,mStrings);
+        }
+
+    }
+    internal class MyDictionaryEnum : IEnumerator
+    {
+        private Int64[] mNums;
+        private String[] mStrings;
+
+        // Enumerators are positioned before the first element
+        // until the first MoveNext() call.
+        int position = -1;
+
+        public MyDictionaryEnum(Int64[] intArray, String[] StrArray)
+        {
+            mNums = intArray;
+            mStrings = StrArray;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < mNums.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public dynamic Current
+        {
+            get
+            {
+                try
+                {
+                    return new { num = mNums[position], str = mStrings[position] };
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
---
+++ chapter10/10-2/MyDictionaryExtension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_2
+{
+    public static class MyDictionaryExtension
+    {
+        internal static dynamic IndexOf(this MyDictionary mdic,Int32 key)
+        {
+            return new { num = mdic.Get_mNums()[key],str = mdic.Get_mStrings()[key] };
+        }
+
+    }
+    internal class Entry
+    {
+        static void Main()
+        {
+            var dic = new MyDictionary(new Int64[] { 1, 2, 3, 4, 5 }, new String[] { "aa", "bb", "cc", "dd", "ee" });
+            //test override ToString.
+            Console.WriteLine(dic.ToString());
+            //test this[] override method
+            Console.WriteLine(dic[0]);//don't forget import Microsoft.CSharp because this[] return a dynamic
+            try
+            {
+                Console.WriteLine(dic[5]);//test IndexOutOfRangeException
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());//log error
+            }
+            //test Extension method
+            Console.WriteLine(dic.IndexOf(1));
+            //test IEnumerable Interface method
+            foreach (var item in dic)//when controlflow goto this line, it will create a temporary IEnumerator for MyDictionary
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
