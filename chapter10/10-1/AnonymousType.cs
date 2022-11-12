15588b4719fd29914459e97b6c98f7188655223e
yueyi <1481796592@qq.com>
2022-11-12 15:44:15 +0800

    AnonymousType

---
+++ chapter10/10-1/AnonymousType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class AnonymousType
+    {
+        static void Main()
+        {
+            var AnonymousObject = new { Name = "name", age = 123 };
+            Console.WriteLine(AnonymousObject.GetType());
+            Console.WriteLine(AnonymousObject.ToString());
+            Type type = AnonymousObject.GetType();
+            var AnonymousObject2 = new { Name = "name", age = 123 , Value = AnonymousObject };
+            Console.WriteLine(AnonymousObject2.GetType());
+            Console.WriteLine(AnonymousObject2.ToString());
+            var AnonymousObject3 = new { Name = "name", age = 123 };
+            Console.WriteLine(AnonymousObject3.GetType());
+            Console.WriteLine(AnonymousObject3.ToString());
+        }
+    }
+}
