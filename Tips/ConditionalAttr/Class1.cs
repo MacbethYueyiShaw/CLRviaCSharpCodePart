70d83c3fa2a3093b6c9ee3d094bcc15acee8e807
Yix <yix@gritworld.com>
2022-11-14 11:23:52 +0800

    add /tips

---
+++ Tips/ConditionalAttr/Class1.cs
@@ -0,0 +1,35 @@
+﻿//#define DEF_DEBUG
+//#define Conditional_DEBUG
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Project1
+{
+    class Class1
+    {
+        static void Main()
+        {
+#if DEF_DEBUG
+            test1();
+#endif
+            test2();
+        }
+#if DEF_DEBUG
+        static void test1()
+        {
+            Console.WriteLine("test1");
+        }
+#endif
+        [Conditional("Conditional_DEBUG")]
+        static void test2()
+        {
+            Console.WriteLine("test2");
+        }
+    }
+}
