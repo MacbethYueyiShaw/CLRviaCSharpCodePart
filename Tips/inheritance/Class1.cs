1739bb1b6a5c7df477250c9ab60bbbcec5528c6d
yueyi <1481796592@qq.com>
2022-11-20 16:45:50 +0800

    test

---
+++ Tips/inheritance/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    internal class Class1
+    {
+        protected Class1()
+        {
+            Console.WriteLine("parent ctor");
+        }
+    }
+    internal class Class2 : Class1
+    {
+        protected Class2()
+        {
+            Console.WriteLine("child ctor");
+        }
+    }
+    internal class Class3 : Class2
+    {
+        public Class3()
+        {
+            Console.WriteLine("child ctor");
+        }
+    }
+    class entry
+    {
+        static void Main()
+        {
+            Class3 class3 = new Class3();
+        }
+    }
+}
