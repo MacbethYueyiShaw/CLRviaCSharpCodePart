75dd69b23bd292ab3a4d15ad0a1615aa4d810669
Yix <yix@gritworld.com>
2023-02-17 15:09:59 +0800

    compare two method to catch exception

--- Tricky/CancelTask/Entry.cs
+++ Tricky/CancelTask/Entry.cs
@@ -11,16 +11,32 @@ namespace CancelTask
     {
         public static void Main()
         {
+            try
+            {
+                Test().Wait();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Main catch exception", e.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Main func return");
+            }
+        }
+        public static async Task Test()
+        {
             var cts = new CancellationTokenSource();
             var ct = cts.Token;
 
-            var mInitializationTask = Task.Run(
+            //1.use task method to parse exception
+            /*var mInitializationTask = Task.Run(
                  () =>
                  {
                      ct.ThrowIfCancellationRequested();
                      Console.WriteLine("mInitializationTask start");
-                    //simulate short-time task
-                    Thread.Sleep(1000);
+                     //simulate short-time task
+                     Thread.Sleep(1000);
                      Console.WriteLine("mInitializationTask finish");
                  }
                  , ct);
@@ -49,25 +65,65 @@ namespace CancelTask
                         }
                     }
                 }
-                , ct);
+                , ct);*/
 
             Thread.Sleep(2000);
             try
             {
                 cts.Cancel();
-                //Thread.Sleep(2000);
-                Task.WaitAll(new Task[] { mInitializationTask, mConcurrentTask });
+                //1.Task method to wait
+                //cts.Cancel();
+                //Task.WaitAll(new Task[] { mInitializationTask, mConcurrentTask });
                 //mConcurrentTask.Wait();
+
+                //2.await method
+                await Task.Run(
+                 () =>
+                 {
+                     ct.ThrowIfCancellationRequested();
+                     Console.WriteLine("mInitializationTask start");
+                     //simulate short-time task
+                     Thread.Sleep(1000);
+                     Console.WriteLine("mInitializationTask finish");
+                 }
+                 , ct);
+                await Task.Run(
+                    () =>
+                    {
+                        Console.WriteLine("mConcurrentTask start");
+                        while (true)
+                        {
+                            if (ct.IsCancellationRequested)
+                            {
+                                try
+                                {
+                                    ct.ThrowIfCancellationRequested();
+                                }
+                                catch (OperationCanceledException e)
+                                {
+                                    Console.WriteLine("catch TaskCanceledException in mConcurrentTask" + e.ToString());
+                                    throw;
+                                }
+                                finally
+                                {
+                                    Console.WriteLine("mConcurrentTask finish");
+                                }
+                            }
+                        }
+                    }
+                    , ct);
             }
             catch (TaskCanceledException e)
             {
                 //try hold TaskCanceledException in cancel part
-                Console.WriteLine("catch TaskCanceledException in main thread cancel code part" + e.ToString());
+                Console.WriteLine("Test: catch TaskCanceledException" + e.ToString());
+                throw;
             }
             catch (AggregateException e)
             {
                 //try hold AggregateException in cancel part
-                Console.WriteLine("catch AggregateException in main thread cancel code part" + e.ToString());
+                Console.WriteLine("Test: catch AggregateException in main thread cancel code part" + e.ToString());
+                throw;
             }
         }
     }
